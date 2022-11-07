a57e2a17776a4ea1db24bcb67383738f2aa842d7
Bruno Gomes <74106187+brunaogomes11@users.noreply.github.com>
2022-11-07 17:15:13 -0300

    Add files via upload

--- app/iaModel.py
+++ app/iaModel.py
@@ -4,6 +4,22 @@ import torch
 import time
 import matplotlib.pyplot as plt
 
+class Net(torch.nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(Net, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
+        self.relu = torch.nn.ReLU()
+        self.fc2 = torch.nn.Linear(self.hidden_size, 1)
+        self.Tanh = torch.nn.Tanh() 
+    def forward(self, x):
+        hidden = self.fc1(x)
+        relu = self.relu(hidden)
+        output = self.fc2(relu)
+        output = self.Tanh(output)
+        return output
+
 def redeNeural(nome, momentum, lr, epocas, hiddenSize, datasetNome, entradas):
     entradas = entradas.replace("[", "")
     entradas = entradas.replace("]", "")
@@ -14,7 +30,7 @@ def redeNeural(nome, momentum, lr, epocas, hiddenSize, datasetNome, entradas):
     hiddenSize = int(hiddenSize)
     start = time.perf_counter()
     datasetNome = datasetNome+'.csv'
-    columns_data = ['HomeTeam', 'AwayTeam', 'FTR']
+    columns_data = ['HomeTeam', 'AwayTeam', 'FTR', 'FTHG', 'FTAG']
     for entrada in entradas:
         columns_data.append(entrada)
     data = pd.read_csv('app/data/dataset/'+datasetNome, header=0, sep=',', usecols=columns_data)
@@ -26,8 +42,8 @@ def redeNeural(nome, momentum, lr, epocas, hiddenSize, datasetNome, entradas):
     test = test.sample(frac=1)
 
     nomes = data[['HomeTeam', 'AwayTeam']]
-    # mediaStats(data)
-    data1 = data.iloc[:, 3:]
+    preverRodada(data, entradas, [])
+    data1 = data.iloc[:, 5:]
     data2 = data[['FTR']]
     data_transformed = data2.replace({"H":1,"D":0,"A":-1})
     input = data1.replace({"H":1,"D":0,"A":-1})
@@ -50,22 +66,6 @@ def redeNeural(nome, momentum, lr, epocas, hiddenSize, datasetNome, entradas):
     test_input = input[-10:]
     test_output = output[-10:]
 
-    # Classe do modelo da Rede Neural
-    class Net(torch.nn.Module):
-        def __init__(self, input_size, hidden_size):
-            super(Net, self).__init__()
-            self.input_size = input_size
-            self.hidden_size = hidden_size
-            self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
-            self.relu = torch.nn.ReLU()
-            self.fc2 = torch.nn.Linear(self.hidden_size, 1)
-            self.Tanh = torch.nn.Tanh() 
-        def forward(self, x):
-            hidden = self.fc1(x)
-            relu = self.relu(hidden)
-            output = self.fc2(relu)
-            output = self.Tanh(output)
-            return output
     # Convertendo para tensor
     training_input = torch.FloatTensor(training_input.values)
     training_output = torch.FloatTensor(training_output.values)
@@ -103,35 +103,42 @@ def redeNeural(nome, momentum, lr, epocas, hiddenSize, datasetNome, entradas):
     erro_pos_treinamento = criterion(y_pred.squeeze(), test_output.squeeze())
     predicted = y_pred.detach().numpy()
     real = test_output.numpy()
-    plotcharts(test_output, y_pred, errors, nomes_test)
     torch.save(model.state_dict(), "app/data/modeloTreinado.pth")
     erro_pos_treinamento = erro_pos_treinamento.item()/len(test_output)
     return tempo_total, erro_pos_treinamento, predicted, real
-
-def plotcharts(test_output, y_pred, errors, nomes_test):
-    errors = np.array(errors)
-    plt.figure(figsize=(12, 5))
-    graf02 = plt.subplot(1, 2, 1) # nrows, ncols, index
-    graf02.set_title('Errors')
-    plt.plot(errors, '-')
-    plt.xlabel('Epochs')
-    graf03 = plt.subplot(1, 2, 2)
-    graf03.set_title('Tests')
-    a = plt.plot(test_output.numpy(), 'yo', label='Real')
-    plt.setp(a, markersize=10)
-    a = plt.plot(y_pred.detach().numpy(), 'b+', label='Predicted')
-    plt.setp(a, markersize=10)
-    xx = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    for x, home, away in zip(xx, nomes_test['HomeTeam'], nomes_test['AwayTeam']):
-      plt.text(x, 1.3, home, rotation='vertical')
-      plt.text(x, -1.3, away, rotation='vertical', verticalalignment='top')
-    plt.legend(loc=0)
-    plt.savefig('app/static/temp/graficoTrain.png', bbox_inches='tight')
 
 # Tratamento String nome arquivo modelo
 def tratarString(nome):
     nome = nome.lower()
     nome = nome.replace(' ', '-')
 
-def testarRedeNeural(jogos):
-    return False
+def preverRodada(input, stats, rodadaInput):
+    times = pd.DataFrame({"HomeTeam":input["HomeTeam"].drop_duplicates()})
+    times["HGA"] = 0
+    media_times = pd.DataFrame({"Time":times["HomeTeam"]})
+    media_times["GolsCasa"] = 0
+    media_times["QntdJogosCasa"] = 0
+    media_times["MediaGolsCasa"] = 0
+    media_times["GolsFora"] = 0
+    media_times["QntdJogosFora"] = 0
+    media_times["MediaGolsFora"] = 0
+    for index, rodada in input.iterrows():
+        for index2, time in times.iterrows():
+            if rodada["AwayTeam"] == time["HomeTeam"]:
+                media_times.iloc[index2, 1] += rodada["FTHG"]
+                media_times.iloc[index2, 2] += 1
+                media_times.iloc[index2, 3] = media_times.iloc[index2, 1]/media_times.iloc[index2, 2]
+                media_times.iloc[index2, 4] += rodada["FTAG"]
+                media_times.iloc[index2, 5] += 1
+                media_times.iloc[index2, 6] = media_times.iloc[index2, 4]/media_times.iloc[index2, 5]
+    media_times2 = pd.DataFrame({"AGH": media_times["MediaGolsCasa"], "AGH": media_times["MediaGolsFora"]})
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    test_input = torch.FloatTensor(media_times2)
+    print(test_input)
+    input_size = test_input.size()[0]
+    hidden_size = 100
+    model = Net(input_size, hidden_size)
+    model.load_state_dict(torch.load('app/data/modelo8-10.pth'))
+    model.eval()
+    y_pred = model(test_input)
+    print(y_pred)
